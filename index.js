b340d45f42906b97cb605eff2cdfc089f4a73b95
Mrumbutsa-Marion <marion.mwanje@student.moringaschool.com>
2023-07-09 10:18:04 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -20,4 +20,62 @@ const allWagesFor = function () {
 
     return payable
 }
+// Your code here
+function createEmployeeRecord (firstName, familyName, title, payPerHour){
+    return{
+        firstName: firstName, 
+        familyName: familyName, 
+        title: title, 
+        payPerHour: PerHour, 
+        timeInEvents:[],
+        timeOutEvents:[]
+    };
+}
+//createEmployeeRecords
+function createEmployeeRecords(employeeData){
+    return employeeData.map(function(data){
+        return createEmployeeRecords(...data);
+    })
+}
 
+//createTimeOutEvent
+
+function createTimeOutEvent(employeeRecord, dataStamp){
+    const [date, hour] = dataStamp.split("");
+    employeeRecord.timeInEvents.push({
+        type: "Timein",
+        hour: parseInt(hour, 10),
+        date: date
+    });
+    return employeeRecord
+}
+
+//hoursWorkedOnDate
+function hoursWorkedOnDate(employeeRecord, date){
+    const timeIn = employeeRecord.employeeRecord.timeInEvents.find(
+        events => event.date === date
+    );
+    const timeOut = employeeRecord.createTimeOutEvent.find(
+         events => event.date === date
+    );
+    return (timeOut.hour - timeIn.hour)/100;
+}
+
+//wagesEarnedOnDate
+function wagesEarnedOnDate(employeeRecord, date){
+    const hoursWorked = hoursWorkedOnDate(employeeRecord, date);
+    const payRate = employeeRecord.payPerHour;
+
+    return hoursWorked * payRate;
+}
+
+//allWagesFor
+
+//calculatePayroll
+function calculatePayroll(employeeRecord){
+    let totalPayRoll =  0;
+    for(const employeeRecord of employeeRecord){
+        totalPayRoll += allWagesFor(employeeRecord);
+    }
+    return totalPayRoll
+}
